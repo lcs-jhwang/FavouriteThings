bc0e13b3743a86dd73ef7ad0ad967d0cbb411bec
Julien Hwang <jhwang@lcs.on.ca>
2024-12-11 11:47:36 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Julien Hwang on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
